88fd5ae5a9298acd3d70a6311456b1f7efa18e99
Gabby Manzano <gabbymanzano98@gmail.com>
2020-10-07 17:32:43 -0500

    change server.js

--- server.js
+++ server.js
@@ -11,41 +11,90 @@ app.use(express.static(__dirname + '/db'));
 
 var notes = require("./db/db.json");
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+// Load data
+const fs = require('fs');
+
+// Routing
+
+module.exports = function(app) {
+  
+  // Routes - one to index, one to notes
+  app.get('/api/notes', function(req, res) {
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      res.send(dbData);
+    });
   });
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-app.get("/api/notes", function(req, res) {
-  return res.json(notes);
-});
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
 
-app.post("/api/notes", function(req, res) {
-  var note = req.body;
-  if(notes.length === 0){
-    Object.assign(note, {"id": "0"})
-  }else{
-    var lastId = parseInt(notes[notes.length -1].id);
-    Object.assign(note, {"id": (lastId + 1).toString()})
-  }
-  console.log(note);
-  notes.push(note);
-  res.json(note);
-});
+  // API POST Requests
 
-app.delete("/api/notes/:id", function(req, res){
-  var chosen = req.params.id;
-  for (var i = 0; i < notes.length; i++){
-    if (chosen === notes[i].id)
-    notes.splice([i],1)
-  }
-  return res.json(notes)
-})
+  app.post('/api/notes', function(req, res) {
+    const userNotes = req.body;
 
-app.listen(PORT, function() {
-  console.log("listening on PORT " + PORT);
-});
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      dbData.push(userNotes);
+      let number = 1;
+      dbData.forEach((note, index) => {
+        note.id = number;
+        number++;
+        return dbData;
+      });
+      console.log(dbData);
+
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.send('Thank you for your note!');
+  });
+
+  // API route to delete notes then update JSON
+  app.delete('/api/notes/:id', function(req, res) {
+    
+    const deleteNote = req.params.id;
+    console.log(deleteNote);
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+
+      // This is to compare each note's id to delete note
+      dbData = JSON.parse(data);
+      
+      for (let i = 0; i < dbData.length; i++) {
+        if (dbData[i].id === Number(deleteNote)) {
+          dbData.splice([i], 1);
+        }
+      }
+      console.log(dbData);
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    // Express response.status everything is running
+    res.status(204).send();
+  });
+};
+const path = require('path');
+
+module.exports = function(app) {
+  
+  app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  // If no matching route is found default to index
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
